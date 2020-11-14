d09e7f302de8850baa45bbc74faab9f1a85e587b
Alexander Pajer <alexander.pajer@integrations.at>
2020-11-14 22:04:00 +0100

    whitespaces

--- Demos/99 FoodApp/FoodUI/src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  apiurl: "https://localhost:5001/api/",
-  appInsights: {
-    instrumentationKey: "a2ba1049-3728-4f4e-bb9e-fcd0d9ac3096",
-  },
-};
--- Demos/99 FoodApp/readme.md
+++
@@ -1,3 +0,0 @@
-# FoodApp
-
-2-tier App using Angular Front End and .NET Core 3.1 Web Api
