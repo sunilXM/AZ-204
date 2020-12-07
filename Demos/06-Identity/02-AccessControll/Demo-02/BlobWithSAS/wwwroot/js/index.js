3cb90fe49d52c6a09f06fff55b6c432de278d96f
Alexander Pajer <alexander.pajer@integrations.at>
2020-12-07 06:52:44 +0100

    blob sas

--- Demos/06-Identity/02-AccessControll/Demo-02/BlobWithSAS/wwwroot/js/index.js
+++ Demos/06-Identity/02-AccessControll/Demo-02/BlobWithSAS/wwwroot/js/index.js
@@ -4,10 +4,21 @@ function getRecords() {
     function (data) {
       $("#result").empty();
       $.each(data, function (i, v) {
-        $("#result").html($("#result").html() + "<p>" + v.name + "<p/>");
+        $("#result").html(
+          `${$("#result").html()}<p onclick='getBlobSAS("${
+            v.name
+          }")' style='cursor:pointer'>${v.name}<p/>`
+        );
       });
       $("#result").addClass("alert alert-success");
     },
     "json"
   );
+}
+
+function getBlobSAS(name) {
+  const sas = "api/PatientRecord/" + name;
+  console.log("calling with SAS:", sas);
+
+  $.get(sas).then((data) => console.log(data));
 }
