b7c3c73a54041296194a97677bff9884cc2792e8
Alexander Pajer <alexander.pajer@integrations.at>
2020-12-08 11:55:47 +0100

    cs

--- Demos/04-CosmosDB/Demo-03/FoodApiCosmos/Database/CosmosDB.cs
+++ Demos/04-CosmosDB/Demo-03/FoodApiCosmos/Database/CosmosDB.cs
@@ -4,7 +4,7 @@ namespace CosmosEF
     {
         string conStr = "AccountEndpoint=https://localhost:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
 
-        string dbname = "";
+        string dbname = "fooddb";
 
         public DbSet<FoodItem> FoodItems { get; set; }
     
